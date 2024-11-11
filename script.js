f968605ed137b72bf0105b9650c21e521650f558
ULISES512 <joseulises.jg@gmail.com>
2024-11-10 20:20:51 -0600

    Ubuntu

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function navigateTo(event, url) {
-    event.preventDefault(); 
-    document.body.classList.add('transicion'); 
-    setTimeout(() => {
-        window.location.href = url; 
-    }, 500); 
+function add(){
+    EncodedVideoChunk
 }
